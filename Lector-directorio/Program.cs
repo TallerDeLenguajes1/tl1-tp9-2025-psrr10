5f1ff2b8a262eac639ab48a22a8792a33c2a489e
Luciano Torres <=>
2025-06-06 21:55:25 -0300

    mostrar carpetas encontradas en un directorio valido

--- Lector-directorio/Program.cs
+++ Lector-directorio/Program.cs
@@ -11,12 +11,26 @@ class Program
         {
             Console.WriteLine("Ingrese un directorio:");
             path = Console.ReadLine();
-            if(!Directory.Exists(path))
+            if (!Directory.Exists(path))
             {
                 Console.WriteLine("Directorio Invalido, ingrese nuevamente:");
 
             }
-        }while(!Directory.Exists(path));
+        } while (!Directory.Exists(path));
         Console.WriteLine("Directorio Valido");
+
+        string[] carpetas = Directory.GetDirectories(path);
+        if (carpetas.Length == 0)
+        {
+            Console.WriteLine("No existen carpetas en el directorio proporcionado");
+        }
+        else
+        {
+                Console.WriteLine("Carpetas encontradas:");
+            foreach (string carpeta in carpetas)
+            {
+                Console.WriteLine("-" + Path.GetFileName(carpeta));
+            }
+        }
     }
 }
