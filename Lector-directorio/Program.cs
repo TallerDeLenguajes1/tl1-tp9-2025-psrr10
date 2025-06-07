b4ff85d866316934856c74ea89a921d746096326
Luciano Torres <=>
2025-06-06 21:46:37 -0300

    solicitar al usuario ingresar path y verificar si es valido o no

---
+++ Lector-directorio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+
+class Program
+{
+    static void Main()
+    {
+        string path;
+        do
+        {
+            Console.WriteLine("Ingrese un directorio:");
+            path = Console.ReadLine();
+            if(!Directory.Exists(path))
+            {
+                Console.WriteLine("Directorio Invalido, ingrese nuevamente:");
+
+            }
+        }while(!Directory.Exists(path));
+        Console.WriteLine("Directorio Valido");
+    }
+}
