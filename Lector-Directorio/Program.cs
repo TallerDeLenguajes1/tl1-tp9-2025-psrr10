4a6c8e29cb22bf5ee205251ce48368fa387c4300
Paulo Srur <paulosrur777@gmail.com>
2025-07-07 16:11:28 -0300

    solicitar al usuario que ingrese el path de un directorio a analizar y archivo gitignore creado

---
+++ Lector-Directorio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path;
+        do
+        {
+            Console.WriteLine("Ingrese el path del directorio a analizar: ");
+            path = Console.ReadLine() ?? "";
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("El directorio ingresado no existe, ingrese nuevamente.");
+            }
+        } while (!Directory.Exists(path));
+        Console.WriteLine($"\nDirectorio válido ingresado: {path}");
+    }
+}
